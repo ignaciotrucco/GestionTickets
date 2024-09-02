48d609d8212aa64b550596ef26c8de8c27362479
ignaciotrucco <truccoignacio9@gmail.com>
2024-09-02 16:58:38 -0300

    modelo tareas

---
+++ GestionTicket/Models/Tarea.cs
@@ -0,0 +1,17 @@
+public class Tarea {
+    public int TareaID {get; set;}
+    public TipoTarea TipoTarea {get; set;}
+    public int UsuarioID {get; set;}
+    public int TiempoEstimado {get; set;}
+    public string DetalleTarea {get; set;}
+    public string? Observaciones {get; set;}
+}
+
+public enum TipoTarea {
+    Desarrollo = 1,
+    Testing,
+    Cobranzas,
+    AtencionYCapacitacion,
+    Migraciones,
+    Implementaciones
+}
