4075c9d60c62fe48b76863fecb5cb8851d702b47
Luciano Festa <lucianoefesta@hotmail.com>
2024-09-06 17:33:42 -0300

    columna tareasimple

--- GestionTicket/Migrations/20240906203132_CampoTareaSimpleTipoTarea.Designer.cs
+++ GestionTicket/Migrations/20240906203132_CampoTareaSimpleTipoTarea.Designer.cs
@@ -4,6 +4,7 @@ using GestionTicket.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestionTicket.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240906203132_CampoTareaSimpleTipoTarea")]
+    partial class CampoTareaSimpleTipoTarea
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GestionTicket/Models/TipoTarea.cs
+++ GestionTicket/Models/TipoTarea.cs
@@ -5,6 +5,7 @@ public class TipoTarea {
     [Key]
     public int TipoTareaID {get; set;}
     public string Nombre {get; set;}
+    public bool TareaSimple {get; set;}
     public bool Eliminado {get; set;}
     public ICollection<Tarea> Tareas {get; set;}
 }
