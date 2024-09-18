1d6a1aa8bb532b98ccb45c6938775847ef03a228
Luciano Festa <lucianoefesta@hotmail.com>
2024-09-18 17:53:15 -0300

    crear tarea inicial

--- GestionTicket/Controllers/HomeController.cs
+++ GestionTicket/Controllers/HomeController.cs
@@ -2,6 +2,10 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GestionTicket.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using GestionTicket.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GestionTicket.Controllers;
 
@@ -10,14 +14,54 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private ApplicationDbContext _context;
+
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager, ApplicationDbContext context)
     {
         _logger = logger;
+
+        _userManager = userManager;
+
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        var tiposTareas = _context.TipoTareas.Where(t => t.Eliminado == false).ToList();
+        tiposTareas.Add(new TipoTarea{
+            TipoTareaID = 0,
+            Nombre = "[Seleccione...]",
+            TareaSimple = true
+        });
+        ViewBag.TipoTarea = new SelectList(tiposTareas.OrderBy(t => t.Nombre), "TipoTareaID", "Nombre");
+
+        var userId = _userManager.GetUserId(User);
+        ViewBag.UsuarioId = userId;
+
         return View();
+    }
+
+    public JsonResult CrearTarea(string tituloTarea, int tipoTarea, string userId)
+    {
+        if(string.IsNullOrEmpty(tituloTarea) || tipoTarea == 0 || string.IsNullOrEmpty(userId))
+        {
+            return Json(new { success = false, text = "Falta completar campos." });
+
+        }else{
+
+            var nuevaTarea = new Tarea {
+                TituloTarea = tituloTarea,
+                TipoTareaID = tipoTarea,
+                UsuarioID = userId
+            };
+
+            _context.Tareas.Add(nuevaTarea);
+            _context.SaveChanges();
+
+            return Json(true);
+        }
     }
 
     public IActionResult Privacy()
--- GestionTicket/wwwroot/js/site.js
+++ GestionTicket/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', () => {
+
+})
+
+function crearTarea(){
+    var tituloTarea = document.getElementById('TituloTarea').value;
+    var tipoTarea = document.getElementById('TipoTarea').value;
+    var userId = document.getElementById('UserId').value;
+
+    $.ajax({
+        url: '../../Home/CrearTarea',
+        data: { tituloTarea, tipoTarea, userId },
+        type: 'POST',
+        dataType: 'json',
+        success: function(result){
+            console.log(result);
+        },
+        error: function(xrs, status){
+            console.log('Ocurrió un error a la hora de almacenar la tarea.')
+        }
+    })
+}
